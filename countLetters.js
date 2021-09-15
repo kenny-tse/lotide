229e3470e134b30d1e6f28be9f00d100b203f442
kenny-tse <kennytse91@gmail.com>
2021-09-15 23:53:06 +0000

    added functionality to countLEtters

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(someString) {
+
+  const lettersToOutput = {};
+  let stringWithoutSpaces = someString.replace(/\s+/g, '');
+  let stringToSort = [];
+
+  for (let i = 0; i < stringWithoutSpaces.length; i++) {
+    stringToSort.push(stringWithoutSpaces[i]);
+  }
+
+  for (let i = 0; i < stringToSort.length; i++) {
+    
+    //if it already exists
+    if (lettersToOutput[stringToSort[i]]) {
+      lettersToOutput[stringToSort[i]] = lettersToOutput[stringToSort[i]] + 1;
+    } else {
+      lettersToOutput[stringToSort[i]] = 1;
+    }
+  }
+
+  console.log(lettersToOutput);
+  return lettersToOutput;
+
+};
+
+countLetters("lighthouse in the house");
+
