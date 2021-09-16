cb0de293cce243216874ba3ba2315413298b0acb
kenny-tse <kennytse91@gmail.com>
2021-09-16 20:23:09 +0000

    fixed assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -11,54 +11,27 @@ const eqArrays = function(arrayOne, ArrayTwo) {
   return true;
 };
 
-const flatten = function(arrayToFlatten) {
-
-  let arrayToReturn = [];
-
-  for (let i = 0; i < arrayToFlatten.length; i++) {
-    if (Array.isArray(arrayToFlatten[i])) {
-      for (let innerIndex = 0; innerIndex < arrayToFlatten[i].length; innerIndex++) {
-        arrayToReturn.push(arrayToFlatten[i][innerIndex]);
-      }
-    } else {
-      arrayToReturn.push(arrayToFlatten[i]);
-    }
-  }
-  return arrayToReturn;
-};
-
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
 
-  let arrayOfKeysObject1 = [];
-  let arrayOfValuesObject1 = [];
+  for(const key in object1){
 
-  let arrayOfKeysObject2 = [];
-  let arrayOfValuesObject2 = [];
+    if(Array.isArray(object1[key])){
 
+      const isEqual = eqArrays(object1[key], object2[key]);
 
-  for (const key in object1) {
-    arrayOfKeysObject1.push(key);
-    arrayOfValuesObject1.push(object1[key]);
+      if(!isEqual){
+        return false;
+      }
+    } else {
+      if(object1[key] !== object2[key]){
+       return false;
+     }
+    }
   }
 
-  for (const key in object2) {
-    arrayOfKeysObject2.push(key);
-    arrayOfValuesObject2.push(object2[key]);
-  }
-
-  let flatAndSortedKeys1 = flatten(arrayOfKeysObject1).sort();
-  let flatAndSortedKeys2 = flatten(arrayOfKeysObject2).sort();
-
-  let flatAndSortedValues1 = flatten(arrayOfValuesObject1).sort();
-  let flatAndSortedValues2 = flatten(arrayOfValuesObject2).sort();
-  
-  if (eqArrays(flatAndSortedKeys1, flatAndSortedKeys2) === true && eqArrays(flatAndSortedValues1, flatAndSortedValues2) === true) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 };
 
 const assertObjectsEqual = function(actual, expected) {
@@ -86,3 +59,7 @@ assertObjectsEqual(cd, dc); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertObjectsEqual(cd, cd2); // => false
+
+const ah = {a: 1, b: [1,2]};
+const ha = {b: 2, a: [1,1]};
+assertObjectsEqual(ha, ah); // => should be false
