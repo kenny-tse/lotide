bec6e3acf33bad41e7d75327c59f02766f4b5453
kenny-tse <kennytse91@gmail.com>
2021-09-17 23:11:35 +0000

    added check for key length

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = function(arrayOne, ArrayTwo) {
+const eqArrays = function (arrayOne, ArrayTwo) {
   if (arrayOne.length !== ArrayTwo.length) {
     return false;
   } else {
@@ -13,38 +13,41 @@ const eqArrays = function(arrayOne, ArrayTwo) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
+const eqObjects = function (object1, object2) {
 
-  for(const key in object1){
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false
+  }
 
-    if(Array.isArray(object1[key])){
+  for (const key in object1) {
+
+    if (Array.isArray(object1[key])) {
 
       const isEqual = eqArrays(object1[key], object2[key]);
+      console.log("this is isEqual: ", isEqual);
 
-      if(!isEqual){
+      if (!isEqual) {
         return false;
       }
     } else {
-      if(object1[key] !== object2[key]){
-       return false;
-     }
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
     }
   }
 
   return true;
 };
 
-const assertObjectsEqual = function(actual, expected) {
+const assertObjectsEqual = function (actual, expected) {
   const inspect = require('util').inspect;
 
-  if (eqObjects(actual,expected)) {
+  if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
-
-assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }); // true
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
@@ -60,6 +63,6 @@ assertObjectsEqual(cd, dc); // => true
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertObjectsEqual(cd, cd2); // => false
 
-const ah = {a: 1, b: [1,2]};
-const ha = {b: 2, a: [1,1]};
+const ah = { a: 1, b: [1, 2] };
+const ha = { b: 2, a: [1, 1] };
 assertObjectsEqual(ha, ah); // => should be false
