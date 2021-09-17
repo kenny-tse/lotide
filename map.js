20ac58b0fd5d8f2db6c5599fb8ed57c6270c6fc0
kenny-tse <kennytse91@gmail.com>
2021-09-17 00:55:31 +0000

    added functionality to map

---
+++ map.js
@@ -0,0 +1,36 @@
+const assertArraysEqual = function(arrayOne, ArrayTwo) {
+  
+  if (eqArrays(arrayOne, ArrayTwo)) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed");
+  }
+};
+
+const eqArrays = function(arrayOne, ArrayTwo) {
+  if (arrayOne.length !== ArrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== ArrayTwo[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(results1, results1);
