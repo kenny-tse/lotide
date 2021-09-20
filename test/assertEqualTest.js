18aeb585d68fca5e4d23b4e7e68af23c73354314
kenny-tse <kennytse91@gmail.com>
2021-09-20 19:38:40 +0000

    seperated tests for assertEqual and head into test folder

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("one", "two");
+assertEqual(99, 99);
+assertEqual(15, 30);
