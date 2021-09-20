6cdefb951bf1d9aab4eb8c9efd0cbd4a7771fd6f
kenny-tse <kennytse91@gmail.com>
2021-09-20 21:09:34 +0000

    adjusted middle and tail to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,20 +1,29 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-middle([1, 2, 3]); // => [2]
-assertArraysEqual(middle([1, 2, 3]), [2]);
+describe("#tail", () => {
+  it("returns [2] when passing array [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-middle([1, 2, 3, 4, 5]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it("returns [3] when passing array [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
 
-middle([1, 2, 3, 4]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+  it("returns [2, 3] when passing array [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
 
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it("returns [3, 4] when passing array [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 
-middle([]); // => []
-assertArraysEqual(middle([]), []);
+  it("returns [] when passing an empty array", () => {
+    assert.deepEqual(middle([]), []);
+  });
 
-middle([1]); // => []
-assertArraysEqual(middle([1]), []);
+  it("returns [] when passing an array with 1 element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,24 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns length 2 when passing array of length 3", () => {
+    assert.equal(tail(["a", "b", "c"]).length, 2);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns [Lighthouse, Labs] when passing [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns [Lighthouse] as the first element when passing [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse");
+  });
+
+  it("returns [Labs] as the second element when passing [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs");
+  });
+
+});
+
+
