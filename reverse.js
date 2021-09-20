138e9419f8c15db3d95b56e2097d1379886ffe01
kenny-tse <kennytse91@gmail.com>
2021-09-20 21:26:43 +0000

    added reverse function

---
+++ reverse.js
@@ -0,0 +1,19 @@
+
+const reverse = function(array) {
+
+  for (let indexWords = 0; indexWords < array.length; indexWords++) {
+
+    let wordBackwards = "";
+
+    for (let indexOfLetters = array[indexWords].length - 1; indexOfLetters >= 0; indexOfLetters--) {
+
+      wordBackwards = wordBackwards + (array[indexWords][indexOfLetters]);
+
+    }
+    console.log(wordBackwards);
+  }
+};
+
+const args = process.argv;
+reverse(args.splice(2));
+
